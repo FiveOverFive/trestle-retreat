d70339f4b33c609afd981d15dbd1160fb1c1ace7
Joe Bacigalupo <joebacigalupo@gmail.comgit config --global user.namegit config --global user.emailgit config --global user.name Joe>
2026-02-01 13:09:09 -0700

    Updated copyright year to 2026

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home() {
       <footer className="bg-gray-50 border-t border-gray-200">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="text-center text-gray-600">
-            <p>&copy; 2024 Trestle Retreat. All rights reserved.</p>
+            <p>&copy; 2026 Trestle Retreat. All rights reserved.</p>
           </div>
         </div>
       </footer>
